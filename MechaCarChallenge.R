a7cfe8de3aa9bd25b3f2eca026c5af5a9cbf26ac
Sara Obermeier <saramcel@gmail.com>
2022-10-12 16:21:46 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,9 +1,13 @@
 # DELIVERABLE 1
 
 library(dplyr)
+# all coefficients included in the following linear model
 mecha_car <- read.csv(file="MechaCar_mpg.csv", check.names=F, stringsAsFactors=F)
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_car) #generate multiple linear regression model
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_car)) #summarize linear model
+model1 <-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_car) #generate multiple linear regression model
+summary(model1) #summarize linear model
+# only the significant results from the first model are included as coefficients here
+model2 <- lm(mpg ~ vehicle_length + ground_clearance,data=mecha_car) #generate multiple linear regression model
+summary(model2) #summarize linear model
 # create scatterplots
 pairs(mecha_car) #this makes scatter plots, but they don't look good
 #install and load the GGally library (suggested here: https://www.statology.org/multiple-linear-regression-r/#:~:text=The%20basic%20syntax%20to%20fit%20a%20multiple%20linear,can%20fit%20the%20model%20using%20the%20following%20code%3A)
@@ -23,11 +27,7 @@ plt <- ggplot(coils,aes(x=Manufacturing_Lot,y=PSI)) #import dataset into ggplot2
 plt + geom_boxplot() + #add boxplot
   theme(axis.text.x=element_text(angle=45,hjust=1)) + #rotate x-axis labels 45 degrees
   geom_point() #overlay scatter plot on top
-# ANOVA
-compare_lots <- coils[,c("Manufacturing_Lot","PSI")] #filter columns from  dataset
-aov(PSI ~ Manufacturing_Lot,data=coils) #compare means across multiple levels
-summary(aov(PSI ~ Manufacturing_Lot,data=coils))
-TukeyHSD(aov(PSI ~ Manufacturing_Lot,data=coils),conf.level=.95)
+
 
 # DELIVERABLE 3
 
